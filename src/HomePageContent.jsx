fd01e29d92be8d50a06e9f353835b62b2dfc9186
jjddean <jkdproductivity@gmail.com>
2025-07-23 14:15:14 +0100

    Reduce spacing between globe icon and 1MarketLive brand text

--- src/HomePageContent.jsx
+++ src/HomePageContent.jsx
@@ -156,7 +156,7 @@ const HomePageContent = () => {
     <div className="min-h-screen bg-gray-100 font-sans text-gray-800">
       {/* Top Navigation Bar */}
       <nav className="w-full bg-white border-b border-gray-200 py-3 flex items-center justify-between">
-        <div className="flex items-center space-x-2 ml-2"> {/* Changed ml-0 to ml-2 for slight indent */}
+        <div className="flex items-center space-x-1 ml-2"> {/* Reduced space-x-2 to space-x-1 for closer spacing */}
           {/* World/Globe SVG Icon */}
           <span className="h-8 w-8 flex items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 text-black">
